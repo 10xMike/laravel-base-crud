da6883e0af35feadfac8718e79d1f61ae4969aa4
10xMike <michelegec@gmail.com>
2021-07-24 20:48:32 +0200

    Install Laravel UI, compile assets

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/pages', function(){
+    return view('pages.index');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
